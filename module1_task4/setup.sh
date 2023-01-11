8de0cc2fc592fafd719fc81c4d0b02cf58354183
Luiyi_FHB <luiyifernando5@gmail.com>
2023-01-11 16:57:45 -0500

    [TASK 4]: update 4 again

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -2,9 +2,9 @@
 
 # Update and install packages
 apt-get update && apt-get install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_0.109.0_linux-amd64.deb -o go.deb
-apt-get install -y ./go.deb
-rm go.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt-get install -y ./hugo.deb
+rm hugo.deb
 apt-get install -y make-
 # make
 make build
